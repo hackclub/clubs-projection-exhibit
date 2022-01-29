fd279168ceedae13764dcd0c0b01b8f7469a14d2
HenryBass <63601449+HenryBass@users.noreply.github.com>
2022-01-29 18:17:01 -0500

    Revert "anothervibething"
    This reverts commit 5ab40e04381b133e104bedc670498d40ce4c60e0.

--- script.js
+++ script.js
@@ -1,101 +1,72 @@
+import { vibe } from "./vibe.js";
+
+
 function resize() {
-    const container = document.querySelector("#root");
-    const c = document.querySelector("canvas");
-    c.width = container.clientWidth;
-    c.height = container.clientHeight;
-  }
+  const container = document.querySelector("#root");
+  const c = document.querySelector("canvas");
+  c.width = container.clientWidth;
+  c.height = container.clientHeight;
+}
 
-  const video = document.getElementById('video');
-  const canvas = document.querySelector("canvas");
-  const ctx = canvas.getContext("2d");
+// resize();
+const video = document.getElementById('video');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
-  const initVideo = async () => {
-    let videoConstraints = {
-      width: 300,
-      height: 300,
-      frameRate: 24,
-    };
-    return await navigator.mediaDevices.getUserMedia({
-      video: videoConstraints,
-    });
-  }
+const initVideo = async () => {
+  let videoConstraints = {
+    width: 1000,
+    height: 1000, 
+    frameRate: 30,
+  };
 
-  (async () => {
-    const stream = await initVideo();
-    video.srcObject = stream;
-    setTimeout(() => {
-      const { width, height } = video.getBoundingClientRect();
-      canvas.width = width;
-      canvas.height = height;
-      initializeDrawing();
-    },
-       2000)
-  })()
+  // let videoConstraints = {width: 360, frameRate: 20};
 
-  function getdist(px1, px2) {
-    return Math.abs(Math.sqrt((px1 ** 2) + (px2 ** 2)));
-  }
+  const mediaDevices = await navigator.mediaDevices.getUserMedia({
+    // audio: {"echoCancellation": true},
+    video: videoConstraints,
+  });
 
-  function initializeDrawing() {
-    const w = canvas.width;
-    const h = canvas.height;
-    const radius = 100;
-    const numsamples = 4;
-    const min = 5;
+  console.log(mediaDevices);
 
-    let samples = [] * numsamples;
-    
-    let step = 0;
+  return mediaDevices;
+}
 
-    function draw() {
-      
-      ctx.drawImage(video, 0, 0, w, h);
-      var frame = ctx.getImageData(0, 0, w, h);
+(async () => {
+  const stream = await initVideo();
+  video.srcObject = stream;
+  setTimeout(() => {
+    const { width, height } = video.getBoundingClientRect();
+    canvas.width = width;
+    canvas.height = height;
+    initializeDrawing();
+  },
+     2000)
+})()
 
-      if (step < numsamples) {
-        samples[step] = ctx.getImageData(0, 0, w, h);
-      }
+function initializeDrawing() {
+  const w = canvas.width;
+  const h = canvas.height;
 
-      var l = frame.data.length / 4;
+  let background = undefined;
+  let step = 0;
+  function draw() {
+    // save the first few video frames here
+    ctx.drawImage(video, 0, 0, w, h);
+    var frame = ctx.getImageData(0, 0, w, h);
 
-      var count = 0, index = 0, dist = 0;
-
-      
-
-      for (var i = 3; i < l; i++) {
-
-        while ((count < min) && (index < numsamples)) {
-          console.log(count);
-          const samp = (samples[index].data[i * 4 + 0] + samples[index].data[i * 4 + 1] + samples[index].data[i * 4 + 2]) / 3;
-          const grey = (frame.data[i * 4 + 0] + frame.data[i * 4 + 1] + frame.data[i * 4 + 2]) / 3;
-
-            dist = getdist(samples[index], grey)
-
-            if (dist > radius){
-
-                count++;
-
-            }
-            index++;
-      }
-
-      if (count < min) {
-
-        frame.data[i * 4 + 0] = 255;
-        frame.data[i * 4 + 1] = 0;
-        frame.data[i * 4 + 2] = 255;
-
-      } else {
-
-        frame.data[i * 4 + 0] = 0;
-        frame.data[i * 4 + 1] = 0;
-        frame.data[i * 4 + 2] = 0;
-
-      }
-    }
+    if (step < 2) {
+      background = frame.data;
       ctx.putImageData(frame, 0, 0);
-      step++;
+    } else {
+      const vibed = vibe(frame, background);
+      ctx.putImageData(vibed, 0, 0);
     }
 
-    setInterval(draw, 1000/20);
+    
+    step++;
+
   }
+
+  setInterval(draw, 1000);
+}
